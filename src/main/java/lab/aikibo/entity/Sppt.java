c37334a43a96d05fb84b25a98445ee0a29791bd6
tamami <tamami.oka@gmail.com>
2016-07-29 14:43:20 +0700

    init commit

---
+++ src/main/java/lab/aikibo/entity/Sppt.java
@@ -0,0 +1,70 @@
+package lab.aikibo.entity;
+
+public class Sppt {
+
+  private String nop;
+  private String thnPajak;
+  private String nama;
+  private String alamatOp;
+  private BigInteger pokok;
+  private BigInteger denda;
+
+  public Sppt(String nop, String thnPajak, String nama, String alamatOp,
+      BigInteger pokok, BigInteger denda) {
+    this.nop = nop;
+    this.thnPajak = thnPajak;
+    this.nama = nama;
+    this.alamatOp = alamatOp;
+    this.pokok = pokok;
+    this.denda = denda;
+  }
+
+  public String getNop() {
+    return nop;
+  }
+
+  public void setNop(String nop) {
+    this.nop = nop;
+  }
+
+  public String getThnPajak() {
+    return thnPajak;
+  }
+
+  public void setThnPajak(String thnPajak) {
+    this.thnPajak = thnPajak;
+  }
+
+  public String getNama() {
+    return nama;
+  }
+
+  public void setNama(String nama) {
+    this.nama = nama;
+  }
+
+  public String getAlamatOp() {
+    return alamatOp;
+  }
+
+  public void setAlamatOp(String alamatOp) {
+    this.alamatOp = alamatOp;
+  }
+
+  public BigInteger getPokok() {
+    return pokok;
+  }
+
+  public void setPokok(BigInteger pokok) {
+    this.pokok = pokok;
+  }
+
+  public BigInteger getDenda() {
+    return denda;
+  }
+
+  public void setDenda(BigInteger denda) {
+    this.denda = denda;
+  }
+
+}
